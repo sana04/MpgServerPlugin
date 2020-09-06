9ff2b4dcac6c7f117c32b4b47fe6f3142c18db81
UanTan123 <62202857+UanTan123@users.noreply.github.com>
2020-09-06 15:22:30 +0900

    Add files via upload

---
+++ MpgServerPlugin/Patches/RadioPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace MpgServerPlugin.Patches
+{
+    [HarmonyPatch(typeof(Radio))]
+    [HarmonyPatch(nameof(Radio.UseBattery))]
+    internal static class RadioPatch
+    {
+        static bool Prefix(Radio __instance)
+        {
+            if (__instance.ccm.CurClass == RoleType.ChaosInsurgency) return true;
+
+            if (__instance.ccm.CurClass == RoleType.Tutorial) return true;
+
+            return false;
+        }
+    }
+}
---
+++ MpgServerPlugin/Patches/ScpSpeak.cs
@@ -0,0 +1,19 @@
+﻿using Assets._Scripts.Dissonance;
+using HarmonyLib;
+
+namespace MpgServerPlugin.Patches
+{
+    [HarmonyPatch(typeof(DissonanceUserSetup), nameof(DissonanceUserSetup.CallCmdAltIsActive))]
+    class ScpSpeak
+    {
+        public static bool Prefix(DissonanceUserSetup __instance, bool value)
+        {
+            CharacterClassManager ccm = __instance.gameObject.GetComponent<CharacterClassManager>();
+
+            if (ccm.CurClass == RoleType.Scp106 || ccm.CurClass == RoleType.Scp0492 || ccm.CurClass == RoleType.Scp049 || ccm.CurClass.Is939())
+                __instance.MimicAs939 = value;
+
+            return true;
+        }
+    }
+}
